2321a2a3e14f48ad1c3b56776e8357f3b2412959
dwightofhand <164888461+dwightofhand@users.noreply.github.com>
2024-10-06 14:04:40 +0100

    functions added

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -2,3 +2,9 @@ function My_First_Function () {
     var str = "This is the button text!";
     document.getElementById ("Button_Text").innerHTML = str;
 }
+
+function myFunction() {
+    var sentence= "I am learning";
+    sentence += " a lot from the book!";
+    document.getElementById("Concatenate").innerHTML = sentence;
+}
