5a2b01003740b86abba99048e779935e28dda7b4
dwightofhand <164888461+dwightofhand@users.noreply.github.com>
2024-10-06 13:34:00 +0100

    Basic javascript assignment

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,10 @@
+var Sent1 = "This is the beginning of the string";  //2 variables
+var Sent2 = " and this is the end of the string";
+
+window.alert("Wassup people");  //window.alert method
+
+document.write(" I said wassup people!"); //document.write method
+
+document.write(Sent1+Sent2);   //displaying 2 variables with the document.write method
+
+document.write(420*69);  //an expression
