b8850137a32c9bb0177b82e8cdc61f575006f997
dwightofhand <164888461+dwightofhand@users.noreply.github.com>
2024-10-19 12:14:53 +0100

    updates

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -1,10 +1,5 @@
-function My_First_Function () {
-    var str = "This is the button text!";
-    document.getElementById ("Button_Text").innerHTML = str;
-}
-
 function myFunction() {
     var sentence= "I am learning";
-    sentence += " a lot from the book!";
+    sentence += " a lot from this course!";
     document.getElementById("Concatenate").innerHTML = sentence;
 }
